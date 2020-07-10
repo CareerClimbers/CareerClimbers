af5429c0b387f16b173e50d3d68ba51e4a362c38
RavenColEvol <ravilamkoti@gmail.com>
2020-07-10 11:41:47 +0530

    fix: mobile cards on courses

--- Frontend/src/components/Courses/Courses.jsx
+++ Frontend/src/components/Courses/Courses.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link, useLocation} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {Skeleton, Row, Col, Pagination, Empty} from 'antd'
@@ -6,10 +6,19 @@ import {Skeleton, Row, Col, Pagination, Empty} from 'antd'
 import {loadCourses} from '../../redux/actions/courses'
 
 import CourseCard from '../CourseCard/CourseCard'
+import MobileCourseCard from '../Category/HorizontalCard'
 import Title from '../Utils/Title'
 
 
 const Courses = ({courses, loadCourses, loading, len}) => {
+    const [isMobile, setisMobile] = useState(null);
+
+    const trackMobile = () => {
+        window.matchMedia("(max-width:768px)").matches
+        ? setisMobile(true)
+        : setisMobile(false);
+    };
+
     let query = new URLSearchParams(useLocation().search).get('q');
     
     useEffect(() => {
@@ -18,11 +27,14 @@ const Courses = ({courses, loadCourses, loading, len}) => {
         }
         loadCourses(g);
         window.scrollTo(0, 0);
+        trackMobile();
+        window.addEventListener("resize", trackMobile);
+        return () => window.removeEventListener("resize", trackMobile);
     }, [loadCourses, query])
 
     const handlePage = (page) => {
         page -= 1;
-        
+        window.scrollTo(0, 0);
         let g = {
             query: query || '',
             page
@@ -46,7 +58,12 @@ const Courses = ({courses, loadCourses, loading, len}) => {
                             courses.map(course => (
                                 <Col key={course._id} xs={24} sm={24} lg={6} md={8}>
                                     <Link to={`/detail/${course._id}`}>
-                                        <CourseCard src={course.img} title={course.title} rating={course.rating} instructor={course.instructor}/>
+                                        {
+                                            isMobile ?
+                                            <MobileCourseCard src={course.img} title={course.title} rating={course.rating} creator={course.instructor}/>
+                                            : 
+                                            <CourseCard src={course.img} title={course.title} rating={course.rating} instructor={course.instructor}></CourseCard>
+                                        }
                                     </Link>
                                 </Col>
                             ))
